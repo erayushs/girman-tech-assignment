6f28f8e8b71b1c99a25958a5b57715b705e32f37
Ayush Singh <anmol8960@gmail.com>
2025-09-04 22:42:54 +0530

    feat(navbar): add Navbar UI and configure Poppins & Inter fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div className="p-4 flex flex-col items-center">
-      <h1>Hello World</h1>
+    <div>
+      <Navbar />
     </div>
   );
 };
