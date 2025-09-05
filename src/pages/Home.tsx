f618a8bb5cabd2233c448bf465650142084e02f8
Ayush Singh <anmol8960@gmail.com>
2025-09-06 03:32:51 +0530

    feat(search-page): improve filter logic, add not-found state with SVG

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import { users, type User } from "../utils/users";
 const Home = () => {
   const [isTyping, setIsTyping] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const [filteredList, setFilteredList] = useState<User[]>(users);
+  const [filteredList, setFilteredList] = useState<User[]>([]);
 
   const handleTyping = (typing: boolean) => {
     setIsTyping(typing);
@@ -16,6 +16,7 @@ const Home = () => {
   };
 
   useEffect(() => {
+    if (searchTerm.trim() === "") return;
     setFilteredList(
       users.filter(
         (user) => user.first_name.toLowerCase() === searchTerm.toLowerCase()
@@ -36,10 +37,23 @@ const Home = () => {
         <SearchBar onTyping={handleTyping} onSubmit={setSearchTerm} />
 
         {searchTerm && (
-          <div className="mt-[40px] flex flex-wrap gap-[22px]">
-            {filteredList.map((user, index) => (
-              <ProfileCard key={index} userDetails={user} />
-            ))}
+          <div className="mt-[40px] flex flex-col items-center gap-[22px]">
+            {filteredList.length > 0 ? (
+              <div className="flex flex-wrap gap-[22px]">
+                {filteredList.map((user, index) => (
+                  <ProfileCard key={index} userDetails={user} />
+                ))}
+              </div>
+            ) : (
+              <div className="flex flex-col items-center text-gray-500 ">
+                <img
+                  src="/no-results.svg"
+                  alt="No results"
+                  className="lg:w-[472px] lg:h-[402px] w-[291px] h-[278px] mt-[-20px]"
+                />
+                <p>No results found.</p>
+              </div>
+            )}
           </div>
         )}
       </div>
